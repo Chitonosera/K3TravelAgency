082cd64a7f134d48b2c3919d4cfeb89df3734f30
OlegaDance <93116261+OlegaDance@users.noreply.github.com>
2023-11-12 16:01:16 +0200

    add js bot

---
+++ telegrambot/main.js
@@ -0,0 +1,79 @@
+const telegramApi = require('node-telegram-bot-api');
+
+const token = '6657330380:AAHtfeCcno4rtsoXY7BuGnzPM28MjkZgo4o';
+
+const apiUrl = '';
+
+const bot = new telegramApi(token, { polling: true });
+
+const chats = {}; 
+
+const info_options = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: 'Ні блять сміюся !', callback_data: 'not'}],
+        ]
+    })
+        
+};
+const start = () => {
+    bot.setMyCommands([
+        {command: '/start', description: 'Запуск'},
+        {command: '/info', description: 'Інформація'},
+    ]);
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+        if (text === '/start') {
+            chats[chatId] = { phone: null };
+            
+            const startOptions = {
+                reply_markup: JSON.stringify({
+                    keyboard: [
+                        [{ text: 'Так' }],
+                        [{ text: 'Ні' }],
+                    ],
+                    resize_keyboard: true,
+                    one_time_keyboard: true,
+                })
+            };
+            return bot.sendMessage(chatId, 'Ви зараестровані на нашому сайті?', startOptions);
+        }
+
+        if (text === 'Так') {
+            const phoneNumberOptions = {
+                reply_markup: JSON.stringify({
+                    force_reply: true,
+                })
+            };
+            return bot.sendMessage(chatId, 'Будь ласка, введіть свій номер телефону:', phoneNumberOptions);
+           
+          
+        }
+
+        if (text === 'Ні') {
+           
+            return bot.sendMessage(chatId, 'Шкода, що ви не готові. Можливо, наступного разу!');
+        }
+        if (chats[chatId]) {
+            chats[chatId].phone = phone_number;
+            
+            try {
+                const response = await axios.post(apiUrl, {
+                    phone_number: text,
+                });
+                const status = response.data.status;
+                const message = response.data.message;
+                return bot.sendMessage(chatId, `${message}. Статус: ${status}`);
+            } catch (error) {
+                console.error('Помилка при виклику API Laravel:', error.message);
+                return bot.sendMessage(chatId, 'Виникла помилка при перевірці номера телефону. Будь ласка, спробуйте пізніше.');
+            }
+        }
+
+        return bot.sendMessage(chatId, 'Чел, що ти висрав?');
+    });
+};
+
+
+start();
